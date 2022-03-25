d43e88ca1096a5aff05f6fad765f60fbbadd1379
jamiejordanmt <jamie.jordan@hackney.gov.uk>
2022-03-25 13:34:54 +0000

    Get app running in .net 6

--- HousingRepairsSchedulingApi/Startup.cs
+++ HousingRepairsSchedulingApi/Startup.cs
@@ -1,16 +1,10 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using HousingRepairsOnline.Authentication.DependencyInjection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
 namespace HousingRepairsSchedulingApi
@@ -91,7 +85,7 @@ namespace HousingRepairsSchedulingApi
 
             app.UseRouting();
 
-            app.UseSentryTracing();
+            // app.UseSentryTracing();
 
             app.UseAuthentication();
             app.UseAuthorization();
