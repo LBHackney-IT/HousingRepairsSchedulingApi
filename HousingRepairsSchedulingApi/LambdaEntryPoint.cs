86f1082e664a8d0e85f3068ec9fd631a303a8943
jamiejordanmt <jamie.jordan@hackney.gov.uk>
2022-03-28 15:26:24 +0100

    Remove terraform

---
+++ HousingRepairsSchedulingApi/LambdaEntryPoint.cs
@@ -0,0 +1,16 @@
+using System.Diagnostics.CodeAnalysis;
+using Amazon.Lambda.AspNetCoreServer;
+using Microsoft.AspNetCore.Hosting;
+
+namespace HousingRepairsSchedulingApi
+{
+    [ExcludeFromCodeCoverage]
+    public class LambdaEntryPoint : APIGatewayProxyFunction
+    {
+        protected override void Init(IWebHostBuilder builder)
+        {
+            builder
+                .UseStartup<Startup>();
+        }
+    }
+}
