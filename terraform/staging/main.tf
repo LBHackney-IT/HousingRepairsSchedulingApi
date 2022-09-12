32fa25f25ada19b104336a9317c5b9c0dca0d8b0
Callum Macpherson <callum.macpherson@hackney.gov.uk>
2022-09-12 12:05:10 +0100

    Add base terraform config

---
+++ terraform/staging/main.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+  region  = "eu-west-2"
+}
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
+locals {
+  application_name = "housing-repairs-scheduling-api" # The name to use for your application
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source = "registry.terraform.io/hashicorp/aws"
+      version = "~> 4.23.0"
+    }
+  }
+  backend "s3" {
+    bucket  = "terraform-state-housing-staging"
+    encrypt = true
+    region  = "eu-west-2"
+    key     = "services/housing-repairs-scheduling-api/state"
+  }
+}
