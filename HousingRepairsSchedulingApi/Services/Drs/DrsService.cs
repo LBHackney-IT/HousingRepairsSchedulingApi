79440024a04735ec0ae03718d88e7ce0a83814dd
Callum Macpherson <callum.macpherson@hackney.gov.uk>
2022-09-14 09:02:03 +0100

    Remove try catch blocks in DRS service

--- HousingRepairsSchedulingApi/Services/Drs/DrsService.cs
+++ HousingRepairsSchedulingApi/Services/Drs/DrsService.cs
@@ -64,39 +64,30 @@ namespace HousingRepairsSchedulingApi.Services.Drs
                 }
             };
 
-            try
+            var checkAvailabilityResponse = await _drsSoapClient.checkAvailabilityAsync(new checkAvailability(checkAvailability));
+
+            _logger.LogInformation("Called checkAvailabilityAsync for {LocationId}", locationId);
+
+            if (checkAvailabilityResponse?.@return?.theSlots == null)
             {
-                var checkAvailabilityResponse = await _drsSoapClient.checkAvailabilityAsync(new checkAvailability(checkAvailability));
-
-                _logger.LogInformation("Called checkAvailabilityAsync for {LocationId}", locationId);
-
-                if (checkAvailabilityResponse?.@return?.theSlots == null)
-                {
-                    _logger.LogInformation("checkAvailabilityAsync returned an invalid response for {LocationId}", locationId);
-                }
-
-                var appointmentSlots = checkAvailabilityResponse.@return.theSlots
-                    .Where(x => x.slotsForDay != null)
-                    .SelectMany(x =>
-                        x.slotsForDay.Where(y => y.available == availableValue.YES).Select(y =>
-                            new AppointmentSlot
-                            {
-                                StartTime = y.beginDate,
-                                EndTime = y.endDate,
-                            }
-                        )
-                );
-
-                _logger.LogInformation("Called checkAvailabilityAsync for {LocationId} returning {Count} appointment slots", locationId, appointmentSlots?.Count() ?? 0);
-
-                return appointmentSlots;
+                _logger.LogInformation("checkAvailabilityAsync returned an invalid response for {LocationId}", locationId);
             }
-            catch (Exception e)
-            {
-                _logger.LogInformation("An error was thrown when calling _drsSoapClient.checkAvailabilityAsync: {Exception}", JsonSerializer.Serialize(e));
 
-                throw;
-            }
+            var appointmentSlots = checkAvailabilityResponse.@return.theSlots
+                .Where(x => x.slotsForDay != null)
+                .SelectMany(x =>
+                    x.slotsForDay.Where(y => y.available == availableValue.YES).Select(y =>
+                        new AppointmentSlot
+                        {
+                            StartTime = y.beginDate,
+                            EndTime = y.endDate,
+                        }
+                    )
+            );
+
+            _logger.LogInformation("Called checkAvailabilityAsync for {LocationId} returning {Count} appointment slots", locationId, appointmentSlots?.Count() ?? 0);
+
+            return appointmentSlots;
         }
 
         public async Task<int> CreateOrder(string bookingReference, string sorCode, string locationId)
@@ -136,29 +127,20 @@ namespace HousingRepairsSchedulingApi.Services.Drs
 
             LambdaLogger.Log($"Built create order object {bookingReference}, sorCode {sorCode}, locationId {locationId};");
 
-            try
+            var createOrderResponse = await _drsSoapClient.createOrderAsync(new createOrder(createOrder));
+
+            if (createOrderResponse?.@return?.theOrder?.theBookings[0]?.bookingId == null)
             {
-                var createOrderResponse = await _drsSoapClient.createOrderAsync(new createOrder(createOrder));
-
-                if (createOrderResponse?.@return?.theOrder?.theBookings[0]?.bookingId == null)
-                {
-                    _logger.LogInformation("createOrderAsync returned an invalid response for {LocationId} ", locationId);
-                }
-
-                LambdaLogger.Log($"Successfully called createOrderAsync with {bookingReference}. createOrderResponse: {JsonSerializer.Serialize(createOrderResponse)}");
-
-                var result = createOrderResponse.@return.theOrder.theBookings[0].bookingId;
-
-                LambdaLogger.Log($"Returning result after sending work order {bookingReference} to DRS. Response booking ID: {result};");
-
-                return result;
+                _logger.LogInformation("createOrderAsync returned an invalid response for {LocationId} ", locationId);
             }
-            catch (Exception e)
-            {
-                LambdaLogger.Log("An error was thrown when calling _drsSoapClient.createOrderAsync: " + JsonSerializer.Serialize(e));
 
-                throw;
-            }
+            LambdaLogger.Log($"Successfully called createOrderAsync with {bookingReference}. createOrderResponse: {JsonSerializer.Serialize(createOrderResponse)}");
+
+            var result = createOrderResponse.@return.theOrder.theBookings[0].bookingId;
+
+            LambdaLogger.Log($"Returning result after sending work order {bookingReference} to DRS. Response booking ID: {result};");
+
+            return result;
         }
 
         public async Task ScheduleBooking(string bookingReference, int bookingId, DateTime startDateTime, DateTime endDateTime)
@@ -189,16 +171,7 @@ namespace HousingRepairsSchedulingApi.Services.Drs
 
             _logger.LogInformation($"scheduleBooking object for booking reference {bookingReference}: {JsonSerializer.Serialize(scheduleBooking)}");
 
-            try
-            {
-                _ = await _drsSoapClient.scheduleBookingAsync(new scheduleBooking(scheduleBooking));
-            }
-            catch (Exception e)
-            {
-                LambdaLogger.Log("An error was thrown when calling _drsSoapClient.scheduleBookingAsync: " + JsonSerializer.Serialize(e));
-
-                throw;
-            }
+            _ = await _drsSoapClient.scheduleBookingAsync(new scheduleBooking(scheduleBooking));
         }
 
         private async Task OpenSession()
