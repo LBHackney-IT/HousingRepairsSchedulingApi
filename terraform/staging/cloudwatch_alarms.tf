9bbb12baeaac9fca657d4972930ed55498c0a267
Callum Macpherson <callum.macpherson@hackney.gov.uk>
2022-09-14 08:50:08 +0100

    Update cloudwatch Alarms

--- terraform/staging/cloudwatch_alarms.tf
+++ terraform/staging/cloudwatch_alarms.tf
@@ -7,26 +7,46 @@ resource "aws_sns_topic" "housing-repairs-scheduling-canary" {
   name = "housing-repairs-scheduling-canary"
 }
 
-// Matching Exceptions
-// "An error was thrown when calling _drsSoapClient.scheduleBookingAsync"
-// "An error was thrown when calling _drsSoapClient.createOrderAsync"
-// "An error was thrown when calling _drsSoapClient.checkAvailabilityAsync"
-
-resource "aws_cloudwatch_log_metric_filter" "housingrepairsschedulingapi-drs-exceptions-filter" {
-  name           = "HousingRepairsSchedulingAPI DRS Exceptions"
-  pattern        = "An error was thrown when calling _drsSoapClient"
+resource "aws_cloudwatch_log_metric_filter" "housingrepairsschedulingapi-drs-createorderasync-exceptions-filter" {
+  name           = "HousingRepairsSchedulingAPI DRS createOrderAsync Exceptions"
+  pattern        = "createOrderAsync returned an invalid response for"
   log_group_name = local.log_group_name
 
   metric_transformation {
-    name      = "HousingRepairsSchedulingAPI DRS Exceptions"
+    name      = "HousingRepairsSchedulingAPI DRS createOrderAsync Exceptions"
     namespace = local.namespace
     value     = "1"
   }
 }
 
-resource "aws_cloudwatch_metric_alarm" "housingrepairsschedulingapi-drs-exceptions-alarm" {
+resource "aws_cloudwatch_log_metric_filter" "housingrepairsschedulingapi-drs-checkavailabilityasync-exceptions-filter" {
+  name           = "HousingRepairsSchedulingAPI DRS checkAvailabilityAsync Exceptions"
+  pattern        = "checkAvailabilityAsync returned an invalid response for"
+  log_group_name = local.log_group_name
+
+  metric_transformation {
+    name      = "HousingRepairsSchedulingAPI DRS checkAvailabilityAsync Exceptions"
+    namespace = local.namespace
+    value     = "1"
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "housingrepairsschedulingapi-drs-createorderasync-exceptions-alarm" {
   alarm_name          = "housingrepairsschedulingapi-drs-exceptions"
-  metric_name         = aws_cloudwatch_log_metric_filter.housingrepairsschedulingapi-drs-exceptions-filter.name
+  metric_name         = aws_cloudwatch_log_metric_filter.housingrepairsschedulingapi-drs-createorderasync-exceptions-filter.name
+  threshold           = "3"
+  statistic           = "Sum"
+  comparison_operator = "GreaterThanThreshold"
+  datapoints_to_alarm = "1"
+  evaluation_periods  = "1"
+  period              = "30"
+  namespace           = local.namespace
+  alarm_actions       = [aws_sns_topic.housing-repairs-scheduling-canary.arn]
+}
+
+resource "aws_cloudwatch_metric_alarm" "housingrepairsschedulingapi-drs-checkavailabilityasync-exceptions-alarm" {
+  alarm_name          = "housingrepairsschedulingapi-drs-exceptions"
+  metric_name         = aws_cloudwatch_log_metric_filter.housingrepairsschedulingapi-drs-checkavailabilityasync-exceptions-filter.name
   threshold           = "3"
   statistic           = "Sum"
   comparison_operator = "GreaterThanThreshold"
